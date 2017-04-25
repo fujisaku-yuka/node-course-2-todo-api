145294f34f4cbeb721884667ca480df8f2d70839
Yuka <fujisaku@gmail.com>
2017-04-25 21:16:01 +0700

    Add POST /todos route and refactor mangoose

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+// connect
+
+// model
+
+// var newTodo = new Todo({
+//   text: '      Edit this video     '
+// });
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner',
+//   completed: false,
+//   completedAt: 1
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Save todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo', e);
+// });
+
+// var newUser = new User({
+//   email: '   yuka@fujisaku.com'
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log('Save user', doc);
+// }, (e) => {
+//   console.log('Unable to save user', e);
+// });
