04c2acac077cd89071c57d4106b80cfd254e46f9
Yuka <fujisaku@gmail.com>
2017-04-30 17:18:20 +0700

    Fix connection to mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://myusername:mypassword@ds157529.mlab.com:57529/tbaustin-node-todo-api',
+  mlab: 'mongodb://admin:password@ds123381.mlab.com:23381/todoapp',
   atlas: 'mongodb://admin:fujisaku12345@cluster0-shard-00-00-5wxmg.mongodb.net:27017,cluster0-shard-00-01-5wxmg.mongodb.net:27017,cluster0-shard-00-02-5wxmg.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 }
-mongoose.connect(process.env.MongoDB_URI ? db.atlas : db.localhost);
+mongoose.connect(process.env.MongoDB_URI ? db.mlab : db.localhost);
 
 module.exports = {mongoose};
