f3d0798c3787cbcaa4241294b1c7c44f356df2a6
Yuka <fujisaku@gmail.com>
2017-04-30 21:09:08 +0700

    Fix connection to mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ db = {
   mlab: 'mongodb://admin:password@ds123381.mlab.com:23381/todoapp',
   atlas: 'mongodb://admin:fujisaku12345@cluster0-shard-00-00-5wxmg.mongodb.net:27017,cluster0-shard-00-01-5wxmg.mongodb.net:27017,cluster0-shard-00-02-5wxmg.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 }
-mongoose.connect(process.env.MongoDB_URI ? db.mlab : db.localhost);
+mongoose.connect(process.env.MONGODB_URI ? db.mlab : db.localhost);
 
 module.exports = {mongoose};
