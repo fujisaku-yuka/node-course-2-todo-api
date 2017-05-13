fb550b415c9ba8e6077d7148cdd4af5d945e9cbc
Yuka <fujisaku@gmail.com>
2017-05-13 22:45:43 +0700

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -113,6 +113,19 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  User.findByCredentials(email, password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
